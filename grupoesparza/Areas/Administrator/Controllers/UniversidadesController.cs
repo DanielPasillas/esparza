6d901c75623d48b0930c48e4a396b18d0d9b9586
Daniel Pasillas Rdz <b.pasillas.4@gmail.com>
2017-12-01 15:59:18 -0600

    Eliminación de el Folder Classes. Es mejor implementar los métodos dentro de los mismos controladores (En algunos casos).

---
+++ grupoesparza/Areas/Administrator/Controllers/UniversidadesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using grupoesparza.Models;
+using grupoesparza.Areas.Classes;
+using System.Web;
+using System.Web.Mvc;
+
+namespace grupoesparza.Areas.Administrator.Controllers
+{
+    public class UniversidadesController : Controller
+    {
+        private readonly esparza_dbEntities _dbContext;
+
+        public UniversidadesController(esparza_dbEntities dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //Methods Section.
+        public List<universidades> GetUsers()
+        {
+            return _dbContext.universidades.ToList();
+        }
+
+
+        // GET: Administrator/Universidades
+        public ActionResult Index()
+        {
+            var users = GetUsers();
+            return View("Index", users);
+        }
+    }
+}
